37d30fa1d2499efdd5cadb51785a306f8218c873
Yanovich Liza <yanovich.liza@yandex.ru>
2020-12-19 23:38:31 +0300

    extract loaders

--- AneuMeshLoader.cpp
+++ AneuMeshLoader.cpp
@@ -10,61 +10,65 @@ AneuMeshLoader::loadMesh(const std::string& filename)
 	std::ifstream fileIn;
 	fileIn.open(filename);
 
-
 	if (!fileIn.is_open()) {
 		throw std::runtime_error("File not open!");
 	}
 
-	unsigned int amount1;
-	unsigned int size1;
-	double x, y, z;
-
-	fileIn >> amount1;
-	std::cout << "amount1=" << amount1 << std::endl;
-	fileIn >> size1;
-	std::cout << "size1=" << size1 << std::endl;
-//	Nodes.reserve(amount);
-
-//	for (int i=0; i<amount; i++)
-//	{
-//		filein >> x >> y >> z;
-//		Nodes.push_back(Node(i, x, y, z, false));
-//	}
-
-	if (size1 != 3) {
-		throw std::runtime_error("Wrong size of Node!");
-	}
-
 	Mesh *mesh = new Mesh();
-	for (int i=1; i<=amount1; i++) {
-		fileIn >> x >> y >> z;
-		mesh->addNode(x, y, z, i);
-	}
-
-	unsigned int amount2;
-	unsigned int size2;
-	unsigned int materialId;
-	unsigned int nodeIds[4];
-
-	fileIn >> amount2;
-	std::cout << "amount2=" << amount2 << std::endl;
-	fileIn >> size2;
-	std::cout << "size2=" << size2 << std::endl;
-
-	if (size2 != 4) {
-		throw std::runtime_error("Wrong size of FE!");
-	}
-
-	for (int i=1; i<=amount2; i++) {
-		fileIn >> materialId >> nodeIds[0] >> nodeIds[1] >> nodeIds[2] >> nodeIds[3];
-		mesh->addFiniteElement(i, materialId, nodeIds);
-	}
-
-	unsigned int amount3;
-	unsigned int size3;
-
-
+	loadNodes(fileIn, mesh);
+	loadFiniteElements(fileIn, mesh);
+	loadBoundaryFiniteElements(fileIn, mesh);
 	fileIn.close();
 	return *mesh;
 }
 
+void
+AneuMeshLoader::loadNodes(std::ifstream &fileIn, Mesh* mesh)
+{
+	unsigned int amount;
+	unsigned int size;
+	double x, y, z;
+
+	fileIn >> amount;
+	std::cout << "amount1=" << amount << std::endl;
+	fileIn >> size;
+	std::cout << "size1=" << size << std::endl;
+
+	if (size != 3) {
+		throw std::runtime_error("Wrong size of Node!");
+	}
+
+	for (int i=1; i<=amount; i++) {
+		fileIn >> x >> y >> z;
+		mesh->addNode(x, y, z, i);
+	}
+
+}
+
+void
+AneuMeshLoader::loadFiniteElements(std::ifstream &fileIn, Mesh* mesh)
+{
+	unsigned int amount;
+	unsigned int size;
+	unsigned int materialId;
+	unsigned int nodeIds[4];
+
+	fileIn >> amount;
+	std::cout << "amount2=" << amount << std::endl;
+	fileIn >> size;
+	std::cout << "size2=" << size << std::endl;
+
+	if (size != 4) {
+		throw std::runtime_error("Wrong size of FE!");
+	}
+
+	for (int i=1; i<=amount; i++) {
+		fileIn >> materialId >> nodeIds[0] >> nodeIds[1] >> nodeIds[2] >> nodeIds[3];
+		mesh->addFiniteElement(i, materialId, nodeIds);
+	}
+}
+
+void
+AneuMeshLoader::loadBoundaryFiniteElements(std::ifstream &fileIn, Mesh* mesh)
+{
+}
--- AneuMeshLoader.h
+++ AneuMeshLoader.h
@@ -8,4 +8,7 @@ public:
 	AneuMeshLoader () {};
 	~AneuMeshLoader () {};
 	virtual Mesh& loadMesh(const std::string& filename) override;
+	void loadNodes(std::ifstream &fileIn, Mesh* mesh);
+	void loadFiniteElements(std::ifstream &fileIn, Mesh* mesh);
+	void loadBoundaryFiniteElements(std::ifstream &fileIn, Mesh* mesh);
 };
